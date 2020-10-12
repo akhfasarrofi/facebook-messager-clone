8e81541afb859bcdc7644473002207de1ec9e807
ZakariaAkhfas <zakaria_akhfas@outlook.co.id>
2020-10-12 10:13:54 +0700

    mengganti apikey firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    apiKey: "AIzaSyDafmSvoZqGVq6ncX5iTG5_qQte2Kbsn9g",
-    authDomain: "facebook-messeger-clone-c5635.firebaseapp.com",
-    databaseURL: "https://facebook-messeger-clone-c5635.firebaseio.com",
-    projectId: "facebook-messeger-clone-c5635",
-    storageBucket: "facebook-messeger-clone-c5635.appspot.com",
-    messagingSenderId: "441103276234",
-    appId: "1:441103276234:web:a9e9aa30a643c604dcfc33",
-    measurementId: "G-JWZYS54GFE"
+    apiKey: "AIzaSyCrFFDx62swfDDktnL39HfX9k1G7Fj4kgY",
+    authDomain: "messager-clone-64384.firebaseapp.com",
+    databaseURL: "https://messager-clone-64384.firebaseio.com",
+    projectId: "messager-clone-64384",
+    storageBucket: "messager-clone-64384.appspot.com",
+    messagingSenderId: "396175517209",
+    appId: "1:396175517209:web:00a418574c6c6e84cd2c88",
+    measurementId: "G-VXP1T8FVXN"
 });
 
 const db = firebaseApp.firestore();
